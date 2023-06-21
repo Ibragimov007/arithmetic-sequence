fd3206bc87e301c52efc0922cfd2914b129e7ee1
Zafarbek_Ibragimov <135506182+Ibragimov007@users.noreply.github.com>
2023-06-21 17:22:34 +0500

    calculated

---
+++ ArithmeticSequence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ArithmeticSequenceTask
+{
+    public static class ArithmeticSequence
+    {
+        public static int Calculate(int number, int add, int count)
+        {
+            if (count < 0)
+            {
+                throw new ArgumentException("Count must be greater than or equal to zero.");
+            }
+
+            if ((add > 0 && number > int.MaxValue - (add * (count - 1))) ||
+                (add < 0 && number < int.MinValue - (add * (count - 1))))
+            {
+                throw new OverflowException("Arithmetic sequence sum exceeds the range of int data type.");
+            }
+
+            int sum = count * ((2 * number) + (add * (count - 1))) / 2;
+            return sum;
+        }
+    }
+}
